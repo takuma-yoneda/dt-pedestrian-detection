13fde01b83e088d2d542598fc89ca019c20af1b1
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-06-19 08:23:50 -0500

    mostly unimportant changes

--- packages/pedestrian-detection/include/pedestrian_detection/detect.py
+++ packages/pedestrian-detection/include/pedestrian_detection/detect.py
@@ -27,146 +27,138 @@ def colorize_segmentation(segm):
         [0, 0, 0],  # black: anything else
     ], dtype=np.uint8)
 
-    ROS_INFO(f'segmentation input shape: {segm.shape}')  # (1, 7, 56, 56)
-    segm = segm.squeeze()
-    out = preset_colors[segm.argmax(axis=0)]
-    ROS_INFO(f'segmentation output shape: {out.shape}')  # (56, 56, 3)
+    segm = segm.squeeze()  # (1, 7, 56, 56)
+    out = preset_colors[segm.argmax(axis=0)]  # (56, 56, 3)
     return out
 
 
-def load_model(path, device, pedest_only=False):
+def load_model(path: str, device):
+    """Load each state dict"""
     from pedestrian_detection.model import make_model
-    if pedest_only:
-        raise NotImplementedError
-
-    model = make_model().to(device)
+    joint_model = make_model().to(device)
     state_dicts = torch.load(path, map_location=device)
-    key2module = {'encoder': model.encoder,
-                  'pedest_lane': model.pedest.lane_head,
-                  'lanepos_offset': model.lanepos.offset_head,
-                  'lanepos_phi': model.lanepos.phi_head,
-                  'segm': model.segment.segm_head}
+    key2module = {
+        "encoder": joint_model.encoder,
+        "pedest_lane": joint_model.pedest.lane_head,
+        "lanepos_offset": joint_model.lanepos.offset_head,
+        "lanepos_phi": joint_model.lanepos.phi_head,
+        "segm": joint_model.segment.segm_head
+    }
     for key, module in key2module.items():
         module.load_state_dict(state_dicts[key])
 
-    return model
-
-
-# def main(model_path):
-#     assert torch.cuda.is_available()
-
-#     # Load the pretrained model
-#     ROS_INFO(f'loading pretrained model from {model_path}')
-#     model = load_model(model_path).cuda()
-
-#     # Perform detection
-#     segm = model.segment.predict(img)
-#     offset, phi = model.lanepos.predict(img)
-#     _, pedest = model.pedest.predict(img)
+    return joint_model
 
 
 class Detector:
-    def __init__(self, duckie_name, model_path) -> None:
+    def __init__(self, node_name, duckie_name, model_path) -> None:
+        """
+        Subscriber:
+            ~image (:obj:`sensor_msgs.msg.CompressedImage`): Input image
+        Publishers:
+            ~detection (:obj:`boolStamped`): Pedestrian Detection Flag
+        """
+        from collections import deque
         # assert torch.cuda.is_available()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         if torch.cuda.is_available:
             ROS_INFO('CUDA is available!!')
 
-        rospy.init_node('pedestrian_detection')
+        rospy.init_node(node_name)
+
 
         # Load the pretrained model
         ROS_INFO(f'loading pretrained model from {model_path}...')
         self.model = load_model(model_path, self.device)
         ROS_INFO(f'loading pretrained model from {model_path}...done')
 
+        self.last_stamp = rospy.Time.now()
+        self.process_frequency = 10  # NOTE: not sure if this is reasonable...
+        self.publish_duration = rospy.Duration.from_sec(1.0 / self.process_frequency)
+
         # self.segm_pub = rospy.Publisher("/detection/segmentation", queue_size=1)
         # self.segm_vis_pub = rospy.Publisher("/detection/segmentation_vis", CompressedImage, queue_size=1)  # Just for visualization
-        self.segm_vis_pub = rospy.Publisher("~segmentation_vis", Image, queue_size=1)  # Just for visualization
-        self.lanepose_pub = rospy.Publisher("~lanepose", LanePose, queue_size=1)
-        self.pedest_pub = rospy.Publisher("~pedestrian", Float32, queue_size=1)
+        # self.segm_vis_pub = rospy.Publisher("~segmentation_vis", Image, queue_size=1)  # Just for visualization
+        # self.lanepose_pub = rospy.Publisher("~lanepose", LanePose, queue_size=1)
+        self.pedest_pub = rospy.Publisher("~detection_score", Float32, queue_size=1)
+        self.pub_detection_flag = rospy.Publisher("~detection", BoolStamped, queue_size=1)
 
-        # DEBUG
-        self.resized_img_pub = rospy.Publisher("~resized_img", Image, queue_size=1)
 
-        # DEBUG
-        # self.reconstr_vis_pub = rospy.Publisher("/detection/reconstr_img__vis", CompressedImage, queue_size=1)
-
-        # TODO: fix it to handle general duckie names. Is there rosparam for it??
+        # TODO: fix it to handle general duckie names. Is there a rosparam for it??
         image_topic = f'/{duckie_name}/camera_node/image/compressed'  # rospy.get_param("~joy_topic", '/joy_teleop/joy')
-        self.img_sub = rospy.Subscriber(image_topic, CompressedImage, self.img_callback, queue_size=1)
+        self.img_sub = rospy.Subscriber(image_topic, CompressedImage, self.cb_image, queue_size=1, buff_size=20 * 1024 ** 2)
+
+        self._filtering = True
+        self._past_detections = deque([False] * 3, maxlen=3)
+
+
+    @staticmethod
+    def process_image(image_cv, dsize, interpolation=cv2.INTER_CUBIC):
+        """
+        Resize, normalize and correct axes of the input cv2 image, so that torch can handle it easily.
+        """
+        image_cv = cv2.resize(image_cv.copy(), dsize=dsize, interpolation=interpolation)
+        image_cv = cv2.cvtColor(image_cv.copy(), cv2.COLOR_BGR2RGB)
+
+        # (width, height, channel) -> (channel, width, height)
+        return image_cv.transpose(2, 0, 1) / 255
 
     @torch.no_grad()
-    def img_callback(self, compr_img_msg):
-        #### direct conversion to CV2 ####
-        # np_arr = np.fromstring(ros_data.data, np.uint8)
-        # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-        # img_msg = ros_data.data
-        _img = cvbridge.compressed_imgmsg_to_cv2(compr_img_msg, desired_encoding="bgr8")
+    def cb_image(self, image_msg):
+        """
+        Callback for processing a image which potentially contains a back pattern. Processes the image only if
+        sufficient time has passed since processing the previous image (relative to the chosen processing frequency).
+        The pattern detection is performed using OpenCV's `findCirclesGrid <https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html?highlight=solvepnp#findcirclesgrid>`_ function.
+        Args:
+            image_msg (:obj:`sensor_msgs.msg.CompressedImage`): Input image
+        """
+        now = rospy.Time.now()
+        if now - self.last_stamp < self.publish_duration:
+            return
+        else:
+            self.last_stamp = now
 
-        # Resize image to (112 x 112) and normalize
-        img = cv2.resize(_img.copy(), dsize=(112, 112), interpolation=cv2.INTER_CUBIC)
-        # img = cv2.resize(_img.copy(), dsize=(112, 112), interpolation=cv2.INTER_AREA)  # INTER_AREA is better than INTER_CUBIC
-        # img = cv2.GaussianBlur(img, (5,5), 0)  # This deteriorates sgementation performance
-        img = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2RGB)
+        image_cv = cvbridge.compressed_imgmsg_to_cv2(image_msg, "bgr8")
 
-        img_resized = cvbridge.cv2_to_imgmsg(img, encoding="rgb8")
-        self.resized_img_pub.publish(img_resized)
+        # TODO: Create a rosparam for dsize?
+        img = self.process_image(image_cv, dsize=(112, 112), interpolation=cv2.INTER_CUBIC)
 
-        # (height, width, channel) -> (channel, width, height)  (?)
-        img = img / 255
-        img = img.transpose(2, 0, 1)
-        import time
-        now = time.time()
         tensor = torch.as_tensor(img)
-        ROS_INFO(f'input tensor dtype {tensor.dtype}')
-        ROS_INFO('hello')
-        # ROS_INFO(f'weight dtype {self.model.segment.encoder.layers[0].weight.dtype}')
         tensor = tensor.unsqueeze(0).float().to(self.device)  # Add batch dim
-        ROS_INFO(f'input device {tensor.device}')
 
-        segm = self.model.segment.predict(tensor)
-        segm_img = colorize_segmentation(segm.cpu().numpy())
+        # segm = self.model.segment.predict(tensor)
+        # segm_img = colorize_segmentation(segm.cpu().numpy())
 
-        offset, phi = self.model.lanepos.predict(tensor)
-        lanepose = LanePose(offset=offset.item(), phi=phi.item())
+        # offset, phi = self.model.lanepos.predict(tensor)
+        # lanepose = LanePose(offset=offset.item(), phi=phi.item())
 
-        _, pedest = self.model.pedest.predict(tensor)
-        ROS_INFO(f'pedest.item(): {pedest.item()}')
-        # pedest = Bool(data=bool(pedest.item()))
-        pedest = pedest.item()
+        _, pedest_prob = self.model.pedest.predict(tensor)
+        ROS_INFO(f'pedest_prob: {pedest_prob.item()}')
+        pedest_prob = pedest_prob.item()
 
-        elapsed = time.time() - now
-        ROS_INFO(f'elapsed: {elapsed}')
+        # if the pattern is detected, cv2.findCirclesGrid returns a non-zero result, otherwise it returns 0
+        # vehicle_detected_msg_out.data = detection > 0
+        # self.pub_detection.publish(vehicle_detected_msg_out)
 
-        # Publish messages
-        # self.segm_pub.publish(msg)
+        detection = pedest_prob > 0.5
+        self._past_detections.appendleft(detection)
 
-        # Create CompressedIamge for segmentation
-        # segm_img_msg = CompressedImage()
-        # segm_img_msg.header.stamp = rospy.Time.now()
-        # segm_img_msg.format = "jpeg"
-        # segm_img_msg.data = np.array(cv2.imencode('.jpg', segm_img)[1]).tostring()
-        # segm_img_msg = cvbridge.cv2_to_compressed_imgmsg(segm_img)
+        detection_flag_msg = BoolStamped()
+        detection_flag_msg.header = image_msg.header
+        if not self._filtering:
+            detection_flag_msg.data = detection
+        else:
+            detection_flag_msg.data = all(self._past_detections)
 
-        # NOTE: Look at https://github.com/whats-in-a-name/CarND-Capstone/commit/de9ad68f4e5f1f983dd79254a71a51894946ac11
-        segm_img_msg = cvbridge.cv2_to_imgmsg(segm_img, encoding="rgb8")
+        self.pub_detection_flag.publish(detection_flag_msg)
 
-        # TEST
-        # img = cvbridge.compressed_imgmsg_to_cv2(compr_img_msg, desired_encoding="bgr8")
-        # ROS_INFO(f'original image shape: {_img.shape}')
-        # ROS_INFO(f'original image dtype: {_img.dtype}')
-        # reconstructed_img_msg = cvbridge.cv2_to_compressed_imgmsg(_img)
-        # reconst_img_msg = CompressedImage()
-        # reconst_img_msg.header.stamp = rospy.Time.now()
-        # reconst_img_msg.format = "jpeg"
-        # reconst_img_msg.data = np.array(cv2.imencode('.jpg', _img)[1]).tostring()
-
-        # self.reconstr_vis_pub.publish(reconst_img_msg)
 
         # Publish new image
-        self.segm_vis_pub.publish(segm_img_msg)
-        self.lanepose_pub.publish(lanepose)
-        self.pedest_pub.publish(pedest)
+        # NOTE: Look at https://github.com/whats-in-a-name/CarND-Capstone/commit/de9ad68f4e5f1f983dd79254a71a51894946ac11
+        # segm_img_msg = cvbridge.cv2_to_imgmsg(segm_img, encoding="rgb8")
+        # self.segm_vis_pub.publish(segm_img_msg)
+        # self.lanepose_pub.publish(lanepose)
+        self.pedest_pub.publish(pedest_prob)
 
     @torch.no_grad()
     def test_pretrained_model(self, data_dir):
